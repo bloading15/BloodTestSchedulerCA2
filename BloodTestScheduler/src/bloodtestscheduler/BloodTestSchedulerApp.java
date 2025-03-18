3496eb248ec97b150471d6b14b58c96943887343
bloading15 <107973283+bloading15@users.noreply.github.com>
2025-03-18 16:55:17 +0000

    App class that contains manually inputed patients and no shows

--- BloodTestScheduler/src/bloodtestscheduler/BloodTestSchedulerApp.java
+++ BloodTestScheduler/src/bloodtestscheduler/BloodTestSchedulerApp.java
@@ -20,7 +20,7 @@ public class BloodTestSchedulerApp {
         Scheduler scheduler = new Scheduler();
         NoShowTracker noShowTracker = new NoShowTracker();
 
-        // Add hardcoded patients
+        // Add patients
         scheduler.addPerson(new Person("Alice Johnson", "Urgent", "Dr. Smith", 45, false));
         scheduler.addPerson(new Person("Bob Brown", "Medium", "Dr. Taylor", 67, true));
         scheduler.addPerson(new Person("Catherine Davis", "Low", "Dr. Allen", 30, false));
@@ -29,6 +29,12 @@ public class BloodTestSchedulerApp {
 
         // Save hardcoded patients to file
         FileHandler.savePatientsToFile(scheduler.getQueue());
+        
+         // Add no-show patients
+        noShowTracker.addNoShow(new Person("Frank Green", "Urgent", "Dr. White", 55, false));
+        noShowTracker.addNoShow(new Person("Grace Hall", "Low", "Dr. Brown", 22, true));
+        noShowTracker.addNoShow(new Person("Hannah Moore", "Medium", "Dr. Taylor", 65, false));
+
 
         // Launch the GUI
         java.awt.EventQueue.invokeLater(() -> {
